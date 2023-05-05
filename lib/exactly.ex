c53da6f64dd9176a6f98bc1eb668c695a2df6f1b
Michael Berkowitz <michael.berkowitz@gmail.com>
2023-05-04 21:05:48 -0400

    Add Duration
    * struct
    * to_string
    * inspect
    * to_lilypond
    
    Also
    ====
    * Add `Utils` module for `frexp/1`
    * Add CI workflow

--- lib/exactly.ex
+++ lib/exactly.ex
@@ -1,2 +1,3 @@
 defmodule Exactly do
+  def to_lilypond(x), do: Exactly.ToLilypond.to_lilypond(x)
 end
---
+++ lib/exactly/duration.ex
@@ -0,0 +1,60 @@
+defmodule Exactly.Duration do
+  defstruct [:log, :dots]
+
+  import Exactly.Utils, only: [frexp: 1]
+
+  @type t :: %__MODULE__{
+          log: integer(),
+          dots: non_neg_integer()
+        }
+
+  def new(value) when is_number(value) do
+    case is_printable(value) do
+      true ->
+        {mant, exp} = frexp(value)
+        log = 1 - exp
+        {m, e} = frexp(1 - mant)
+        dots = -e - if m > 0.5, do: 1, else: 0
+        %__MODULE__{log: log, dots: dots}
+
+      false ->
+        {:error, :unprintable_duration, value}
+    end
+  end
+
+  defimpl String.Chars do
+    @named_durations ~w(breve longa maxima)
+    def to_string(%Exactly.Duration{log: log, dots: dots}) do
+      base_duration =
+        case log do
+          n when n < 0 -> "\\" <> Enum.at(@named_durations, -1 - log)
+          _ -> Bitwise.bsl(1, log)
+        end
+
+      "#{base_duration}#{String.duplicate(".", dots)}"
+    end
+  end
+
+  defimpl Inspect do
+    import Inspect.Algebra
+
+    def inspect(%Exactly.Duration{} = duration, _opts) do
+      concat([
+        "#Exactly.Duration<",
+        to_string(duration),
+        ">"
+      ])
+    end
+  end
+
+  defimpl Exactly.ToLilypond do
+    def to_lilypond(%Exactly.Duration{} = duration) do
+      to_string(duration)
+    end
+  end
+
+  defp is_printable(value) when is_number(value) do
+    {mant, exp} = frexp(value)
+    -10 < exp && exp < 5 && elem(frexp(1 - mant), 0) == 0.5
+  end
+end
